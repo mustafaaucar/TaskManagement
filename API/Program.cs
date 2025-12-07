46c7a32dcb2fcc852627e3053c6f553f074b1ac5
Mustafa UÇAR <67073181+mustafaaucar@users.noreply.github.com>
2025-12-07 15:39:53 +0300

    program cs güncellendi

--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Infrastucture.Mongo;
 using Microsoft.Extensions.Configuration;
 using Application.Behaviors;
+using Application.Tasks.Commands.UpdateTask;
+using Application.Tasks.Commands.DeleteTask;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +35,18 @@ app.MapPost("/tasks", async (CreateTaskCommand cmd, IMediator mediator)
 
 app.MapGet("/tasks", async (IMediator mediator)
     => Results.Ok(await mediator.Send(new GetAllTasksQuery())));
+
+
+app.MapPut("/tasks/{id}/status", async (string id, bool completed, IMediator mediator) =>
+{
+    var result = await mediator.Send(new UpdateTaskCommand(id, completed));
+    return result ? Results.NoContent() : Results.NotFound();
+});
+app.MapDelete("/tasks/{id}", async (string id, IMediator mediator) =>
+{
+    var result = await mediator.Send(new DeleteTaskCommand(id));
+    return result ? Results.NoContent() : Results.NotFound();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
